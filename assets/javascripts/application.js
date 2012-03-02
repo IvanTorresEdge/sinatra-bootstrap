713c275a6c07ba75750cde22f5b3c53b19c0035d
Ivan Torres <mexpolk@gmail.com>
2012-03-01 20:55:58 -0600

    upgrade to Backbone.js 0.9.1

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,5 +1,5 @@
 //= require vendor/jquery-1.7.1.js
 //= require vendor/json2.js
-//= require vendor/underscore-1.3.0.js
-//= require vendor/backbone-0.5.3.js
+//= require vendor/underscore-1.3.1.js
+//= require vendor/backbone-0.9.1.js
 //= require hello.js
